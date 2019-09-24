40b157d587ad5fae17076405b8d79fdcec226c8a
郑丹 <1003572640@qq.com>
2019-09-24 12:17:52 +0800

    调整

--- src/main/java/com/solid/subscribe/web/SpSubscribeWebApplication.java
+++ src/main/java/com/solid/subscribe/web/SpSubscribeWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PermManageApplication {
+public class SpSubscribeWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PermManageApplication.class, args);
+        SpringApplication.run(SpSubscribeWebApplication.class, args);
     }
 
 }
